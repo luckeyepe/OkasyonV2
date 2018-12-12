f3c0ee5185c15a819ae5054e4ba758d54cace051
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2018-12-12 19:10:58 +0800

    Partial

---
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.morkince.okasyonv2.activities.signup_client_activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.morkince.okasyonv2.R
+
+class SignUpClientPart2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_client_part2)
+    }
+}
---
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SingUpSummaryClientActivity.kt
@@ -0,0 +1,13 @@
+package com.example.morkince.okasyonv2.activities.signup_client_activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.morkince.okasyonv2.R
+
+class SingUpSummaryClientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up_summary_client)
+    }
+}
