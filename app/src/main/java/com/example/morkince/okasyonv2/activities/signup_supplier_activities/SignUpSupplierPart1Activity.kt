80f77e6e611dab93ff2890d45944b460d6ee9b22
Japhet Titus Abanto <ondoyjet2@gmail.com>
2018-12-12 19:16:47 +0800

    files to be added

--- app/src/main/java/com/example/morkince/okasyonv2/activities/signup_supplier_activities/SignUpSupplierPart1Activity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_supplier_activities/SignUpSupplierPart1Activity.kt
@@ -1,13 +1,23 @@
 package com.example.morkince.okasyonv2.activities.signup_supplier_activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.morkince.okasyonv2.R
+import com.example.morkince.okasyonv2.activities.login_activities.SignUpSupplierPart2
+import kotlinx.android.synthetic.main.activity_sign_up_supplier_part1.*
 
 class SignUpSupplierPart1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_supplier_part1)
+
+        SupplierPart1ImgViewBttnNext.setOnClickListener {
+            val intent =Intent(this@SignUpSupplierPart1Activity, SignUpSupplierPart2 :: class.java)
+            startActivity(intent)
+        }
+
     }
 }
