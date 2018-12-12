c3d2b6599a18830ba037a222a9678a51b60c3427
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2018-12-12 20:15:00 +0800

    SignUpClient

--- app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart1Activity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart1Activity.kt
@@ -1,13 +1,20 @@
 package com.example.morkince.okasyonv2.activities.signup_client_activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.morkince.okasyonv2.R
+import kotlinx.android.synthetic.main.activity_sign_up_client_part1.*
 
 class SignUpClientPart1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_client_part1)
+        imagebutton_signupClientPart1Next.setOnClickListener {
+            val intent = Intent(this, SignUpClientPart2Activity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart2Activity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart2Activity.kt
@@ -1,13 +1,37 @@
 package com.example.morkince.okasyonv2.activities.signup_client_activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.morkince.okasyonv2.R
+import kotlinx.android.synthetic.main.activity_sign_up_client_part2.*
 
 class SignUpClientPart2Activity : AppCompatActivity() {
+    var gender = arrayOf( "Male", "Female")
 
+    var spinner: Spinner? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_client_part2)
+        imageButton_signupclientPart2BackButton.setOnClickListener {
+            val intent = Intent(this, SignUpClientPart1Activity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+        imageButton_signupclientPart2NextButton.setOnClickListener {
+            val intent = Intent(this,SingUpSummaryClientActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+        spinner = this.Spinner_signupclientpart2Gender
+
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, gender)
+        // Set layout to use when the list of choices appear
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinner!!.setAdapter(aa)
     }
 }
