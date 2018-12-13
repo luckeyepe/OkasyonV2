075233e229a2b60cf02632136598324514624e29
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2018-12-13 15:41:04 +0800

    continuation of client registration and homepage for Supplier with navigation drawer

--- app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart2Activity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SignUpClientPart2Activity.kt
@@ -1,12 +1,16 @@
 package com.example.morkince.okasyonv2.activities.signup_client_activities
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.example.morkince.okasyonv2.R
 import kotlinx.android.synthetic.main.activity_sign_up_client_part2.*
+import java.util.*
 
 class SignUpClientPart2Activity : AppCompatActivity() {
     var gender = arrayOf( "Male", "Female")
@@ -33,5 +37,19 @@ class SignUpClientPart2Activity : AppCompatActivity() {
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         // Set Adapter to Spinner
         spinner!!.setAdapter(aa)
+    }
+    @SuppressLint("SetTextI18n")
+    fun funDate (view: View){
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+
+        val dpd = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
+            textView_signupClientPart2DateOfBirth.text ="$dayOfMonth, $monthOfYear $year"
+        }, year, month, day)
+
+        //show datepicker
+        dpd.show()
     }
 }
--- app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SingUpSummaryClientActivity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/signup_client_activities/SingUpSummaryClientActivity.kt
@@ -1,13 +1,22 @@
 package com.example.morkince.okasyonv2.activities.signup_client_activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.morkince.okasyonv2.R
+import com.example.morkince.okasyonv2.activities.homepage_supplier_activities.HomePage_Supplier_Activity
+import kotlinx.android.synthetic.main.activity_sign_up_client_part2.*
+import kotlinx.android.synthetic.main.activity_sing_up_summary_client.*
 
 class SingUpSummaryClientActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up_summary_client)
+        imageButton_signupclientPart3NextButton.setOnClickListener {
+            val intent = Intent(this,HomePage_Supplier_Activity ::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
 }
