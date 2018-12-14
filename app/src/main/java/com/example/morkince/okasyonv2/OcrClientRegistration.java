afe8cb337517ddbd45fe39beb5e11d05ca4c6d44
Marc Lennard Colina <marccolina456@gmail.com>
2018-12-14 11:47:20 +0800

    Commit with OCR for client and organizer

--- app/src/main/java/com/example/morkince/okasyonv2/OcrClientRegistration.java
+++ app/src/main/java/com/example/morkince/okasyonv2/OcrClientRegistration.java
@@ -2,7 +2,6 @@ package com.example.morkince.okasyonv2;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.FaceDetector;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -21,99 +20,62 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
 
-public class ocr_supplier_registration extends AppCompatActivity {
+public class OcrClientRegistration extends AppCompatActivity {
 
-
-    ImageView ocr_supplier_valid_id,ocr_supplier_valid_business_permit;
-    ImageButton btn_supplier_valid_id,btn_supplier_valid_business_permit;
-    boolean ifValidIdImageIsSelected=false;
-    boolean ifBusinessPermitImageIsSelected =false;
-    private Uri filePathValidID=null;
-    private Uri filePathBusinessPermit=null;
-    private Bitmap bitmapBusinessPermit;
-    private Bitmap bitmapValidID;
+    ImageView ocr_client_registration_validID_ImgView;
+    ImageButton ocr_client_registration_validID_imageBtn;
+    Button ocr_client_registration_completeRegistrationBtn;
     private static final int MAXIMUM_FACE=1;
     private static FaceDetector.Face[] facesValidID = new FaceDetector.Face[MAXIMUM_FACE];
-    private static FaceDetector.Face[] facesBusinessPermit = new FaceDetector.Face[MAXIMUM_FACE];
+    private Uri filePathValidID=null;
+    private Bitmap bitmapValidID;
     private static final int PICK_IMAGE = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ocr_supplier_registration);
+        setContentView(R.layout.activity_ocr_client_registration);
         refs();
 
-        btn_supplier_valid_id.setOnClickListener(addValidID);
-        btn_supplier_valid_business_permit.setOnClickListener(addBusinesspermit);
+        ocr_client_registration_validID_imageBtn.setOnClickListener(addValidID);
+       // btn_supplier_valid_business_permit.setOnClickListener(addBusinesspermit);
     }
 
-    //ON CLICK LISTENER TO ADD IMAGE TO VALID ID IMAGE VIEW AND CHOOSE FROM GALLERY
     private View.OnClickListener addValidID = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            ifValidIdImageIsSelected=true;
             openGallery();
+
         }
     };
 
-    private View.OnClickListener addBusinesspermit = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            ifBusinessPermitImageIsSelected=true;
-            openGallery();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            //CHECK IF UPLOADING A VALID ID
+            ocr_client_registration_validID_ImgView.setImageResource(R.drawable.camera_icon);
+            filePathValidID = data.getData();
+            try {
+                bitmapValidID = MediaStore.Images.Media.getBitmap(getContentResolver(), filePathValidID);
+                if (recognizeText() && detectFaceValidID()) {
+                    ocr_client_registration_validID_ImgView.setImageBitmap(bitmapValidID);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    };
+    }
 
-
-    //METHOD FOR ADDING IMAGE TO VALID ID IMAGE VIEW AND CHOOSE FROM GALLERY
     public void openGallery()
     {
         Intent gallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery,PICK_IMAGE);
     }
-    //THIS IS TO LOAD THE IMAGES CHOSEN FROM GALLERY TO THE IMAGE VIEWS
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK
-                && data != null && data.getData() != null )
-        {
-            //CHECK IF UPLOADING A VALID ID
-            if(ifValidIdImageIsSelected)
-            {
-                filePathValidID = data.getData();
-                try {
-                     bitmapValidID = MediaStore.Images.Media.getBitmap(getContentResolver(),filePathValidID);
 
 
-
-                    if( recognizeText() &&  detectFaceValidID())
-                        ocr_supplier_valid_id.setImageBitmap(bitmapValidID);
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-
-
-
-            }
-            //CHECK IF UPLOADING A BUSINESS PERMIT
-            else{
-                filePathBusinessPermit = data.getData();
-                try {
-                     bitmapBusinessPermit = MediaStore.Images.Media.getBitmap(getContentResolver(),filePathBusinessPermit);
-                    ocr_supplier_valid_business_permit.setImageBitmap(bitmapBusinessPermit);
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-            ifBusinessPermitImageIsSelected=false;
-            ifValidIdImageIsSelected=false;
-        }
-    }
 
     private boolean detectFaceValidID()
     {
@@ -130,6 +92,7 @@ public class ocr_supplier_registration extends AppCompatActivity {
             }
             else
             {
+                Toast.makeText(getApplication(), "THERE IS  :  0 " + " FACE IN VALID ID", Toast.LENGTH_SHORT).show();
                 return false;
             }
         } catch (IOException e) {
@@ -138,6 +101,7 @@ public class ocr_supplier_registration extends AppCompatActivity {
 
         return false;
     }
+
 
     private boolean recognizeText()
     {
@@ -161,17 +125,21 @@ public class ocr_supplier_registration extends AppCompatActivity {
                     validIDText.append(myItem.getValue());
                     validIDText.append("\n");
                 }
-
-                if(validIDText.toString().contains("ABANTO, JAPHET TITUS H."))
+                Log.e("TEXT : ", validIDText + " HERE");
+                if(validIDText.toString().contains("ABANTO, JAPHET TITUS HINGPIT"))
                 {
                     Toast.makeText(getApplication(), "CONTAINS NAME!! ", Toast.LENGTH_SHORT).show();
-                    Log.e("TEXT : ", validIDText + "");
+
                     return true;
                 }
                 else
                 {
+                    Toast.makeText(getApplication(), "INFORMATION INPUTTED DOES NOT MATCH ID!! ", Toast.LENGTH_SHORT).show();
+
                     return false;
                 }
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -179,12 +147,12 @@ public class ocr_supplier_registration extends AppCompatActivity {
         return false;
     }
 
+
     public void refs()
     {
-        ocr_supplier_valid_id=findViewById(R.id.ocr_registration_supplier_validID_ImgView);
-        ocr_supplier_valid_business_permit=findViewById(R.id.ocr_registration_supplier_busPermit_ImgView);
-        btn_supplier_valid_id=findViewById(R.id.ocr_registration_supplier_validID_imageBtn);
-        btn_supplier_valid_business_permit=findViewById(R.id.ocr_registration_supplier_businessPermit_imgBtn);
+        ocr_client_registration_validID_imageBtn=findViewById(R.id.ocr_client_registration_validID_imageBtn);
+        ocr_client_registration_validID_ImgView=findViewById(R.id.ocr_client_registration_validID_ImgView);
+        ocr_client_registration_completeRegistrationBtn=findViewById(R.id.ocr_client_registration_completeRegistrationBtn);
     }
 
 }
