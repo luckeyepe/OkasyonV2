1ba9d52b5e180188f8737b9bb87c7b506f9beac0
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-01-07 19:41:24 +0800

    merge fix

--- app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/Client_Set_Preference.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/Client_Set_Preference.kt
@@ -1,4 +1,4 @@
-package com.example.morkince.okasyonv2.activities.signup_client_activities
+package com.example.morkince.okasyonv2.activities.client_activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
