0aa06e9143a97223e71e0afb7d6f9cf2de28bc5d
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-02-08 22:39:34 +0800

    [Recommendation] altered the search, and get related items

--- app/src/main/java/com/example/morkince/okasyonv2/activities/HomePage_Client_activity/Client_Viewitems.java
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/HomePage_Client_activity/Client_Viewitems.java
@@ -60,66 +60,44 @@ public class Client_Viewitems extends AppCompatActivity {
         mFunctions = FirebaseFunctions.getInstance();
 
         loadAllItemsInItemCategory(itemCategory);
-
-        // add button listener
-
-
-        // custom dialog
-
-
-//                // set the custom dialog components - text, image and button
-////                TextView text = (TextView) dialog.findViewById(R.id.text);
-////                text.setText("Android custom dialog example!");
-////                ImageView image = (ImageView) dialog.findViewById(R.id.image);
-////                image.setImageResource(R.drawable.ic_launcher);
-
-
-//        Store item1 = new Store();
-//        item1.setStoreName("Marc's Gown");
-//        item1.setLocation("Hernan Cortes");
-//        item1.setPrice(100.00);
-//
-//        Store item2 = new Store();
-//        item2.setStoreName("Japhet's SoundSystems");
-//        item2.setLocation("Nasipit Talamban");
-//        item2.setPrice(200.00);
-//
-//        Store item3 = new Store();
-//        item3.setStoreName("Mikay's Eatery");
-//        item3.setLocation("629 Gov. Cuenco Ave Banilad Cebu City");
-//        item3.setPrice(300.00);
-//
-//
-//        StoreItem.add(item1);
-//        StoreItem.add(item2);
-//        StoreItem.add(item3);
-//
-//        adapter = new ViewItemRecyclerAdapter(StoreItem, Client_Viewitems.this);
-//        recyclerView.setAdapter(adapter);
     }
 
     private void loadAllItemsInItemCategory(String itemCategory) {
-        FirebaseFirestore.getInstance()
-                .collection("Items")
-                .whereEqualTo("item_category_id", itemCategory)
-                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        getRelatedItems(itemCategory).addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
             @Override
-            public void onComplete(Task<QuerySnapshot> task) {
+            public void onComplete(Task<ArrayList<String>> task) {
                 if (task.isSuccessful()){
-                    groupieAdapter.clear();
+                    ArrayList<String> itemUid = task.getResult();
 
-                    for (Item item: task.getResult().toObjects(Item.class)) {
-                        BasicItemViewHolder basicItemViewHolder = new BasicItemViewHolder(item.getItem_uid(),
-                                item.getItem_average_rating(),
-                                item.getItem_price(),
-                                item.getItem_name(),
-                                getApplicationContext(),
-                                item.getItem_display_picture_url());
+                    for (String item: itemUid) {
+                        FirebaseFirestore.getInstance()
+                                .collection("Items")
+                                .document(item)
+                                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(Task<DocumentSnapshot> task) {
+                                com.example.morkince.okasyonv2.activities.model.Item item = task.getResult().toObject(com.example.morkince.okasyonv2.activities.model.Item.class);
+                                BasicItemViewHolder basicItemViewHolder = new BasicItemViewHolder(item.getItem_uid(),
+                                        item.getItem_average_rating(),
+                                        item.getItem_price(),
+                                        item.getItem_name(),
+                                        getApplicationContext(),
+                                        item.getItem_display_picture_url());
 
-                        groupieAdapter.add(basicItemViewHolder);
+                                groupieAdapter.add(basicItemViewHolder);
+                            }
+                        });
                     }
 
                     recyclerView.setAdapter(groupieAdapter);
+
+                }else {
+                    Exception e = task.getException();
+                    if (e instanceof FirebaseFunctionsException) {
+                        FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                        FirebaseFunctionsException.Code code = ffe.getCode();
+                        Object details = ffe.getDetails();
+                    }
                 }
             }
         });
@@ -129,8 +107,6 @@ public class Client_Viewitems extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mi = getMenuInflater();
         mi.inflate(R.menu.menuclientviewitemsactionbar, menu);
-//        mSearchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
-////        setupSearchView();
 
         MenuItem searchItem = menu.findItem(R.id.app_bar_search);
         SearchView vs = (SearchView) searchItem.getActionView();
@@ -150,8 +126,8 @@ public class Client_Viewitems extends AppCompatActivity {
                 // Do your task here
                 groupieAdapter.clear();
 
-                String itemQuery = (itemCategory+" "+query.trim().toLowerCase());
-                getRelatedItems(itemQuery).addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
+                String itemQuery = (query.trim().toLowerCase());
+                searchForItem(itemQuery, itemCategory).addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
                     @Override
                     public void onComplete(Task<ArrayList<String>> task) {
                         if (task.isSuccessful()){
@@ -198,44 +174,6 @@ public class Client_Viewitems extends AppCompatActivity {
         return true;
     }
 
-//    private void setupSearchView() {
-//
-//        mSearchView.setIconifiedByDefault(true);
-//
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        if (searchManager != null) {
-//            List<SearchableInfo> searchables = searchManager.getSearchablesInGlobalSearch();
-//
-//            // Try to use the "applications" global search provider
-//            SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
-//            for (SearchableInfo inf : searchables) {
-//                if (inf.getSuggestAuthority() != null
-//                        && inf.getSuggestAuthority().startsWith("applications")) {
-//                    info = inf;
-//                    Toast.makeText(getApplicationContext(), "This is my Toast message!" + info,
-//                            Toast.LENGTH_LONG).show();
-//                }
-//            }
-//            mSearchView.setSearchableInfo(info);
-//            Toast.makeText(getApplicationContext(), "This is my Toast message!" + info,
-//                    Toast.LENGTH_LONG).show();
-//        }
-//
-//    }
-
-
-    //    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if(id == R.id.Filter)
-//        {
-//            final Dialog dialog = new Dialog(context);
-//            dialog.setContentView(R.layout.activity_filter);
-//            dialog.setTitle("Title...");
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 //        Fragment selectedFragment = null;
@@ -254,8 +192,6 @@ public class Client_Viewitems extends AppCompatActivity {
                 WindowManager.LayoutParams wlp = window.getAttributes();
                 wlp.gravity = Gravity.BOTTOM | Gravity.RIGHT;
                 window.setAttributes(wlp);
-//                wlp.gravity = Gravity.RIGHT;
-//                wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
                 window.setAttributes(wlp);
 
                 return true;
@@ -269,21 +205,33 @@ public class Client_Viewitems extends AppCompatActivity {
         recyclerView = findViewById(R.id.RecyclerView_ClientViewItems);
     }
 
-    private Task<ArrayList<String>> getRelatedItems(String searchQuery) {
-        // Create the arguments to the callable function, which are two integers
+    private Task<ArrayList<String>> getRelatedItems(String itemCategory) {
         Map<String, Object> data = new HashMap<>();
-        data.put("text", searchQuery);
+        data.put("item_category", itemCategory);
 
-        // Call the function and extract the operation from the result
         return mFunctions
                 .getHttpsCallable("getRelatedItems")
                 .call(data)
                 .continueWith(new Continuation<HttpsCallableResult, ArrayList<String>>() {
                     @Override
                     public ArrayList<String> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
-                        // This continuation runs on either success or failure, but if the task
-                        // has failed then getResult() will throw an Exception which will be
-                        // propagated down.
+                        Map<String, Object> result = (Map<String, Object>) task.getResult().getData();
+                        return (ArrayList<String>) result.get("itemUids");
+                    }
+                });
+    }
+
+    private Task<ArrayList<String>> searchForItem(String searchQuery, String itemCategory) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("query", searchQuery);
+        data.put("item_category", itemCategory);
+
+        return mFunctions
+                .getHttpsCallable("getRelatedItems")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, ArrayList<String>>() {
+                    @Override
+                    public ArrayList<String> then(@NonNull Task<HttpsCallableResult> task) throws Exception {
                         Map<String, Object> result = (Map<String, Object>) task.getResult().getData();
                         return (ArrayList<String>) result.get("itemUids");
                     }
