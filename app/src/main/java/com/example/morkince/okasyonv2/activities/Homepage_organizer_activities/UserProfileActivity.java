7b3a0ac99082e81e013286d551516472e6764e63
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2019-02-12 20:32:30 +0800

    searchviewtoast

---
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/Homepage_organizer_activities/UserProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.morkince.okasyonv2.activities.Homepage_organizer_activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.morkince.okasyonv2.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
