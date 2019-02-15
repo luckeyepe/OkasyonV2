8a7d8840b9528293c3f7d42fefaae30e1d35fca3
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2019-02-15 14:05:37 +0800

    Client/organizer and supplier profile

--- app/src/main/java/com/example/morkince/okasyonv2/activities/Homepage_organizer_activities/CartItemDetailsActivity.java
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/Homepage_organizer_activities/CartItemDetailsActivity.java
@@ -1,19 +1,34 @@
 package com.example.morkince.okasyonv2.activities.Homepage_organizer_activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.morkince.okasyonv2.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class CartItemDetailsActivity extends AppCompatActivity {
 Button btndelete;
 TextView Itemprice, Itemquantity, ItemName, ItemDatefrom, ItemDateto;
+    FirebaseUser user;
+    FirebaseFirestore db;
+    StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_item_details);
         refs();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.menu_proceedtransaction,menu);
+        return super.onCreateOptionsMenu(menu);
     }
     public void refs()
     {
