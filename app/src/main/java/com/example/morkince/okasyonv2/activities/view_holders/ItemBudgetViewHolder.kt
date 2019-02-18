ed603cb0c147a66e4ccb9995359c8ae4dbc91615
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-02-18 11:31:08 +0800

    add update budget to item category

--- app/src/main/java/com/example/morkince/okasyonv2/activities/view_holders/ItemBudgetViewHolder.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/view_holders/ItemBudgetViewHolder.kt
@@ -3,12 +3,16 @@ package com.example.morkince.okasyonv2.activities.view_holders
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
 import com.example.morkince.okasyonv2.R
 import com.example.morkince.okasyonv2.activities.client_activities.ClientViewItemsActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.modal_update_set_budget.view.*
 import kotlinx.android.synthetic.main.row_item_budget_summary.view.*
+import java.text.DecimalFormat
 
 class ItemBudgetViewHolder(val itemCategory: String,
                            val budgetSpent: Double,
@@ -33,13 +37,59 @@ class ItemBudgetViewHolder(val itemCategory: String,
             itemCategoryTextView.text = itemCategory
         }
 
+        if (budgetSet<budgetSpent){
+            itemSetBudgetTextView.setTextColor(Color.RED)
+        }
+
         itemSetBudgetTextView.text = budgetSet.toString()
         itemSpentBudgetTextView.text = budgetSpent.toString()
 
 
         itemSetBudgetTextView.setOnClickListener {
             //update budget
+            var dialogBuilder: AlertDialog.Builder?
+            var alert: AlertDialog.Builder?
+            var dialog: AlertDialog?
 
+            //start making the popup after checking the value of the id Number
+            var view = LayoutInflater.from(context).inflate(R.layout.modal_update_set_budget, null)
+
+            var popupBudget = view.editText_updateSetBudgetModalBudget
+            var popupItemCategory = view.textView_updateSetBudgetModalCategory
+            var popupUpdateButton = view.button_updateSetBudgetModalUpdate
+            var popupCancelButton = view.button_updateSetBudgetModalCancel
+
+
+            if (itemCategory.contains("_")){
+                popupItemCategory.text = itemCategory.replace("_", " ")
+            }else{
+                popupItemCategory.text = itemCategory
+            }
+
+            popupBudget.hint ="P ${DecimalFormat("##.##").format(budgetSet)}"
+
+            dialogBuilder = AlertDialog.Builder(context).setView(view)
+            dialog = dialogBuilder!!.create()
+            dialog?.show()
+
+
+            popupUpdateButton.setOnClickListener {
+                //popup info
+                val newBudget = popupBudget.text.toString().trim().toDouble()
+                FirebaseFirestore.getInstance()
+                    .collection("Custom_Event_Item_Category")
+                    .document(eventUid)
+                    .collection("ceic_item_category")
+                    .document(itemCategory)
+                    .update("ceic_item_set_budget", newBudget)
+                    .addOnSuccessListener {
+                        dialog.dismiss()
+                    }
+            }
+
+            popupCancelButton.setOnClickListener {
+                dialog.dismiss()
+            }
         }
 
 
