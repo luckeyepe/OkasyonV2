ce5697f0aa4e0f321f974b5fae04561c80f047bf
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-02-22 14:19:47 +0800

    code clean up

--- app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientViewItemsActivity.java
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientViewItemsActivity.java
@@ -170,6 +170,7 @@ public class ClientViewItemsActivity extends AppCompatActivity {
                 // Do your task here
                 groupieAdapter.clear();
                 String itemQuery = (query.trim().toLowerCase());
+
                 CallableFunctions callableFunctions = new CallableFunctions();
                 callableFunctions.searchForItem(itemQuery, itemCategory)
                         .addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
@@ -266,13 +267,18 @@ public class ClientViewItemsActivity extends AppCompatActivity {
                 saveFilterButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        boolean isForSale=false;
+                        boolean isForSale;
+
+                        groupieAdapter.clear();
+                        dialog.dismiss();
+
                         String storeName = txtStorename.getText().toString().trim().isEmpty() ? "" : txtStorename.getText().toString().trim();
                         Double budget = txtBudget.getText().toString().trim().isEmpty() ? -1 : Double.valueOf(txtBudget.getText().toString().trim());
-                        String location = txtLocation.toString().trim().isEmpty() ? "" :txtLocation.toString().trim();
+                        String location = txtLocation.toString().trim().isEmpty() ? "" :txtLocation.getText().toString().trim();
+
                         int idOfChecked = Radiogroup_forRentorSale.getCheckedRadioButtonId();
 
-                        if(idOfChecked==R.id.radioButton_Buy)
+                        if(idOfChecked == R.id.radioButton_Buy)
                         {
                             isForSale=true;
                         }
@@ -283,45 +289,61 @@ public class ClientViewItemsActivity extends AppCompatActivity {
 
                         Toast.makeText(getApplicationContext(), isForSale + "", Toast.LENGTH_SHORT).show();
 
-
-//                        Boolean isForSale = RadioButton.();
-//                        Toast.makeText(getApplicationContext(), forRent.getText(), Toast.LENGTH_SHORT).show();
-//                        Toast.makeText(getApplicationContext(), forsale.getText(), Toast.LENGTH_SHORT).show();
-
                         int itemScore = Math.round(ratingBar.getRating());
-                        Toast.makeText(getApplicationContext(), itemScore+"", Toast.LENGTH_SHORT).show();
-//                        if(isForSale == true) {
-//                            Toast.makeText(getApplicationContext(), "Buy", Toast.LENGTH_SHORT).show();
-//                            forRent.setEnabled(false);
-////                            isForSale = true;
-//                        }
-//                        else if(forRent.isChecked()){
-//                            Toast.makeText(getApplicationContext(), "rent", Toast.LENGTH_SHORT).show();
-//                            forsale.setEnabled(false);
-//                        }
-//                        Toast.makeText(getApplicationContext(), txtBudget.getText().toString(), Toast.LENGTH_LONG).show();
-//                        Toast.makeText(getApplicationContext(), txtStorname.getText().toString(), Toast.LENGTH_LONG).show();
-//                        Toast.makeText(getApplicationContext(), txtlocation.getText().toString(), Toast.LENGTH_LONG).show();
-//                        CallableFunctions callableFunctions = new CallableFunctions();
 
-//
-//                        callableFunctions.filterItems(itemCategory, storeName, budget, location, itemScore, isForSale)
-//                                .addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<ArrayList<String>> task) {
-//
-//                            }
-//                        });
-//                        forsale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-//
-//                            @Override
-//                            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                                if (forsale.isChecked()){
-//                                    Toast.makeText(getApplicationContext(), "Buy" , Toast.LENGTH_SHORT).show();
-//                                    forsale.setEnabled(false); // disable checkbox
-//                                }
-//                            }
-//                        });
+                        CallableFunctions callableFunctions = new CallableFunctions();
+                        callableFunctions.filterItems(itemCategory, storeName, budget, location, itemScore, isForSale)
+                                .addOnCompleteListener(new OnCompleteListener<ArrayList<String>>() {
+                                    @Override
+                                    public void onComplete(Task<ArrayList<String>> task) {
+                                        if (task.isSuccessful()){
+                                            ArrayList<String> itemUid = task.getResult();
+
+                                            if (itemUid.size() == 0){
+                                                textViewMessage.setVisibility(View.VISIBLE);
+                                                return;
+                                            }else {
+                                                textViewMessage.setVisibility(View.INVISIBLE);
+                                            }
+
+                                            for (int i =0; i<itemUid.size(); i++) {
+                                                FirebaseFirestore.getInstance()
+                                                        .collection("Items")
+                                                        .document(itemUid.get(i))
+                                                        .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(Task<DocumentSnapshot> task) {
+                                                        if (task.isSuccessful()) {
+                                                            com.example.morkince.okasyonv2.activities.model.Item item =
+                                                                    task.getResult().toObject(com.example.morkince.okasyonv2.activities.model.Item.class);
+
+                                                            BasicItemViewHolder basicItemViewHolder = new BasicItemViewHolder(item.getItem_uid(),
+                                                                    item.getItem_average_rating(),
+                                                                    item.getItem_price(),
+                                                                    item.getItem_name(),
+                                                                    event_cart_group_uid,
+                                                                    getApplicationContext(),
+                                                                    item.getItem_display_picture_url(), event_event_uid);
+
+                                                            groupieAdapter.add(basicItemViewHolder);
+                                                        }
+                                                    }
+
+                                                });
+                                            }
+
+                                            recyclerView.setAdapter(groupieAdapter);
+
+                                        }else {
+                                            Exception e = task.getException();
+                                            if (e instanceof FirebaseFunctionsException) {
+                                                FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                                                FirebaseFunctionsException.Code code = ffe.getCode();
+                                                Object details = ffe.getDetails();
+                                            }
+                                        }
+                                    }
+                                });
                     }
 
 
