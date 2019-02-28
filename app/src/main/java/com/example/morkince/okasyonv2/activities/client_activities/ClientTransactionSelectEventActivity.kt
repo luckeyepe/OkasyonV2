68666b7a1ccba5349cdb24dfe3df04fa93f0ecd1
Mikhayl Joshua Castro Abellanosa <luckeyepe@gmail.com>
2019-02-28 11:46:05 +0800

    [Transaction] Created custom popups

--- app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientTransactionSelectEventActivity.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientTransactionSelectEventActivity.kt
@@ -2,7 +2,11 @@ package com.example.morkince.okasyonv2.activities.client_activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.morkince.okasyonv2.Custom_Progress_Dialog
+import com.example.morkince.okasyonv2.PopUpDialogs
 import com.example.morkince.okasyonv2.R
+import com.example.morkince.okasyonv2.RandomMessages
+import com.google.firebase.firestore.FirebaseFirestore
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 
@@ -12,6 +16,30 @@ class ClientTransactionSelectEventActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_transaction_select_event)
 
+        val popupDialog = PopUpDialogs(this)
+        val custom_Progress_Dialog = Custom_Progress_Dialog(this)
+        val randomMessages = RandomMessages()
+
+        custom_Progress_Dialog.showDialog("LOADING", randomMessages.getRandomMessage())
+
         var adapter = GroupAdapter<ViewHolder>()
+
+        FirebaseFirestore.getInstance()
+            .collection("Transaction_Client")
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                if (firebaseFirestoreException != null){
+                    custom_Progress_Dialog.dissmissDialog()
+
+                    popupDialog.errorDialog("Something went wrong", "ERROR")
+
+                    return@addSnapshotListener
+                }
+
+                if (querySnapshot!!.size()>0){
+                    //populate recyler
+                }else{
+
+                }
+            }
     }
 }
--- app/src/main/java/com/example/morkince/okasyonv2/activities/view_holders/BasicEventViewHolder.kt
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/view_holders/BasicEventViewHolder.kt
@@ -1,14 +1,56 @@
 package com.example.morkince.okasyonv2.activities.view_holders
 
+import android.net.Uri
+import com.example.morkince.okasyonv2.Events
+import com.example.morkince.okasyonv2.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.row_basic_event_details.view.*
 
-class BasicEventViewHolder:Item<ViewHolder>() {
+class BasicEventViewHolder(var eventUid: String):Item<ViewHolder>() {
+
     override fun getLayout(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return R.layout.row_basic_event_details
     }
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var eventNameTextView = viewHolder.itemView.textView_basicEventDetailsRowEventName
+        var eventPicture = viewHolder.itemView.imageView_basicEventDetailsRowEventName
+
+        FirebaseFirestore.getInstance()
+            .collection("Event")
+            .document(eventUid)
+            .get()
+            .addOnCompleteListener {
+                task: Task<DocumentSnapshot> ->
+                if (task.isSuccessful){
+                    val result = task.result!!
+                    val eventName = result.get("event_name").toString()
+
+                    eventNameTextView.text = eventName
+
+                    //grab picture
+                    FirebaseStorage.getInstance().reference
+                        .child("event_images")
+                        .child(eventUid)
+                        .downloadUrl
+                        .addOnCompleteListener {
+                            task: Task<Uri> ->
+                            if(task.isSuccessful){
+                                val uri = task.result
+                                Picasso.get().load(uri.toString()).into(eventPicture)
+                            }else{
+                                Picasso.get().load(R.drawable.default_avata).into(eventPicture)
+                            }
+                        }
+                }else{
+                    //
+                }
+            }
     }
 }
