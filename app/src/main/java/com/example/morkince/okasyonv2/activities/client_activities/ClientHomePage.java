ec7eeaae74cf887c462b92339a3ee0d62e303b68
Mikhayl Joshua Castro Abellanosa <luckeyepe@gmail.com>
2019-03-02 21:31:03 +0800

    abanto commit

--- app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientHomePage.java
+++ app/src/main/java/com/example/morkince/okasyonv2/activities/client_activities/ClientHomePage.java
@@ -20,6 +20,7 @@ import com.example.morkince.okasyonv2.Events;
 import com.example.morkince.okasyonv2.R;
 import com.example.morkince.okasyonv2.activities.Homepage_organizer_activities.Activity_Vieworganizer;
 import com.example.morkince.okasyonv2.activities.Homepage_organizer_activities.UserProfileActivity;
+import com.example.morkince.okasyonv2.activities.NotificationsActivity;
 import com.example.morkince.okasyonv2.activities.adapter.EventsAdapter;
 import com.example.morkince.okasyonv2.activities.chat_activities.LatestMessagesActivity;
 import com.example.morkince.okasyonv2.activities.client_fragments.TopEvents_Fragment;
@@ -531,6 +532,8 @@ public void ClientCreateEvent()
              break;
 
          case R.id.nav_notifications:
+             Intent notificationIntent = new Intent(ClientHomePage.this, NotificationsActivity.class);
+             startActivity(notificationIntent);
              break;
 
          case R.id.nav_cart:
